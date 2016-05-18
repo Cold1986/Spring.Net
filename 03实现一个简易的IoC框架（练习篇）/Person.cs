1e78b75488886fb9f513a8a3a3e88e0af4ffa501
13935 <hl13935@ly.com>
2016-05-18 18:15:49 +0800

    新增第三课

---
+++ 03实现一个简易的IoC框架（练习篇）/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 实现一个简易的IoC框架
+{
+    public class Person
+    {
+        public override string ToString()
+        {
+            return "我是Test22";
+        }
+    }
+}
---
+++ 03实现一个简易的IoC框架（练习篇）/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 实现一个简易的IoC框架
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppRegistry();
+            Console.ReadLine();
+        }
+
+        static void AppRegistry()
+        {
+            MyXmlFactory ctx = new MyXmlFactory(System.IO.Path.GetFileName("Objects.xml"));
+            Console.WriteLine(ctx.GetObject("Person").ToString());
+        }
+    }
+}
