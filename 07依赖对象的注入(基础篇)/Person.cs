50fcce82cf269e14ecb5387df6cabbb63ec79a59
13935 <hl13935@ly.com>
2016-05-20 10:00:57 +0800

    新增 07 依赖对象的注入

---
+++ 07依赖对象的注入(基础篇)/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 依赖对象的注入
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person Friend { get; set; }
+    }
+}
---
+++ 07依赖对象的注入(基础篇)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spring.Context;
+using Spring.Context.Support;
+
+namespace 依赖对象的注入
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IApplicationContext ctx = ContextRegistry.GetContext();
+
+            PersonDao dao = ctx.GetObject("personDao") as PersonDao;
+            dao.Get();
+
+            Console.ReadLine();
+        }
+    }
+}
